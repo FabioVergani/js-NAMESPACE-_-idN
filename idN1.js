0401702b51d542cc070925451e589209a060f7cc
FabioVergani <fabio.vergani.1975@gmail.com>
2014-05-26 15:39:22 +0200

    Create idN1.js

---
+++ idN1.js
@@ -0,0 +1,7 @@
+var idN=(function(p){return function(e){var n=e[p]||0;e[p]=++n;return n}})('id-n');
+
+
+console.log(idN(window));
+console.log(idN(window));
+console.log(idN(window));
+console.log(idN(window));
